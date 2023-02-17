55c2116dfc4789f684071046026395d585aa1b86
WellingDevp <kristinwelling18@gmail.com>
2023-02-16 23:56:03 -0800

    new Updates

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - program that prints exactly new line, to the standard error.
+ *
+ * Return: 0 (good)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", stdout);
+	return (1);
+}
