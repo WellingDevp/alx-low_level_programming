5c6bd46efc1c3573bea3c04df2098f21ac56807c
WellingDevp <kristinwelling18@gmail.com>
2023-02-17 00:03:53 -0800

    good

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,8 @@
  * Return: 0 (good)
  */
 int main(void)
+
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", stdout);
 	return (1);
 }
