9bb5cc8990084f075b7798673571ce90265c90d3
WellingDevp <kristinwelling18@gmail.com>
2023-02-17 00:40:56 -0800

    cool

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,12 @@
 #include <unistd.h>
 
 /**
- * main - program that prints exactly new line, to the standard error.
+ * main - program that prints exactly new linie
  *
  * Return: 1 (success)
  */
 int main(void)
-
 {
 	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+return (1);
 }
