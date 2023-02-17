96750ffdb44e7a20d60a46f02e5aca9f0c6f471e
WellingDevp <kristinwelling18@gmail.com>
2023-02-17 00:15:25 -0800

    updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,11 +4,11 @@
 /**
  * main - program that prints exactly new line, to the standard error.
  *
- * Return: 0 (good)
+ * Return: 1 (success)
  */
 int main(void)
 
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
