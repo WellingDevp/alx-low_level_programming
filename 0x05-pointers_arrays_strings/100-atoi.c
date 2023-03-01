639c98394bf2c667f954c46b8b0b02abd33baf52
WellingDevp <kristinwelling18@gmail.com>
2023-02-28 17:25:34 -0800

    updated

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,46 +1,38 @@
 #include "main.h"
 
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
- *
- * Return: the int converted from the string
+ * _atoi - function declaration
+ * @s: string pointer
+ * Return: integer equivalen of string
  */
+
+int _atoi(char *s);
+
+/**
+ * _atoi - function definition
+ * Description: convert string to integer
+ * @s: the string pointer
+ * Return: integer value of string
+ */
+
 int _atoi(char *s)
 {
-	int i, d, n, len, f, digit;
+	int num_sign = 1;
+	unsigned int digit = 0;
 
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
-
-	while (s[len] != '\0')
-		len++;
-
-	while (i < len && f == 0)
-	{
-		if (s[i] == '-')
-			++d;
-
-		if (s[i] >= '0' && s[i] <= '9')
+	do {
+		if (*s == '-')
 		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
+			num_sign *= -1;
 		}
-		i++;
-	}
-
-	if (f == 0)
-		return (0);
-
-	return (n);
+		else if (*s >= '0' && *s <= '9')
+		{
+			digit = (digit * 10) + (*s - '0');
+		}
+		else if (digit > 0)
+		{
+			break;
+		}
+	} while (*s++);
+	return (digit * num_sign);
 }
